3005bf10725863642ba39ba81c91526fd6f5f06b
tablonimus <lautamartin1995@gmail.com>
2023-10-19 23:35:24 -0300

    new route

---
+++ vite-project/src/components/MyNav/MyNav.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./myNavStyles.css";
+import { NavLink } from "react-router-dom";
+
+export default function MyNav() {
+  return (
+    <div className="nav-container">
+      <NavLink
+        to={"/"}
+        className={({ isActive }) => (isActive ? "active-btn" : "disabled-btn")}
+      >
+        {" "}
+        {/* isActive => true o false si esta activa */}
+        Home
+      </NavLink>
+      <NavLink
+        to={"/productos"}
+        className={({ isActive }) => (isActive ? "active-btn" : "disabled-btn")}
+      >
+        Productos
+      </NavLink>
+      <NavLink
+        to={"/sobrenosotros"}
+        className={({ isActive }) => (isActive ? "active-btn" : "disabled-btn")}
+      >
+        Sobre Nosotros
+      </NavLink>
+    </div>
+  );
+}
